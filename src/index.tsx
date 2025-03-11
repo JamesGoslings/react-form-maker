88ea198b834fb98369c8b667818c3c005a110120
JamesGosing <3248175240@qq.com>
2025-03-11 22:36:57 +0800

    refactor: 配置antd中文

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/es/locale/zh_CN";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>
+  </ConfigProvider>
 );
-
