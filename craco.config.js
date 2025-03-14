5d42eb7389c4361f2d92c1d6e7925afbea053a69
JamesGosing <3248175240@qq.com>
2025-03-14 21:23:22 +0800

    feat: 配置路径替换符

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,28 @@
+const path = require('path')
+
 module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    // 添加 resolve 配置
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss', '.css'],
+    },
+  },
+  // 配置 sass-loader
+  style: {
+    sass: {
+      loaderOptions: {
+        sassOptions: {
+          includePaths: [path.resolve(__dirname, 'src')],
+        },
+      },
+    },
+  },
   devServer: {
     port: 3678,
     proxy: {
