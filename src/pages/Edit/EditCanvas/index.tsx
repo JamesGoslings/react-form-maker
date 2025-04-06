08e1024598f0d9742e9f387672ba107439180083
JamesGosing <3248175240@qq.com>
2025-04-06 16:33:41 +0800

    perf: 优化占位组件位置确认逻辑

--- src/pages/Edit/EditCanvas/index.tsx
+++ src/pages/Edit/EditCanvas/index.tsx
@@ -54,14 +54,24 @@ const EditCanvas: FC<PropsType> = function ({ loading }: PropsType) {
   }
 
   /**
+   * 将最后一个组件的下方确认为占位组件
+   */
+  function setEndTmpComponent() {
+    if (componentList.length > 0) {
+      const lastComponent = componentList[componentList.length - 1]
+      setRelativeEleId(lastComponent.fe_id)
+      setPlacement('bottom')
+    }
+  }
+
+  /**
    * 拖拽元素进入其他组件之后的逻辑：确定占位组件
    * @param e
    * @param id 进入的组件 id
    */
   function handleDragEnter(e: DragEvent, id: string) {
-    // e.stopPropagation()
+    e.stopPropagation()
     e.preventDefault()
-
     if (e.target instanceof HTMLElement) {
       setRelativeEleId(id)
       const rect = e.target.getBoundingClientRect()
@@ -125,15 +135,37 @@ const EditCanvas: FC<PropsType> = function ({ loading }: PropsType) {
     }
   }
 
+  /**
+   * 处理进入 container 时的逻辑，
+   * 实现如果是最后一个组件下方进入的，占位组件直接放最后一个
+   * @param e
+   */
+  function handleEnterContainer(e: DragEvent) {
+    e.preventDefault()
+    const canvasContainer = document.getElementById('canvasContainer')
+    if (canvasContainer) {
+      const lastComponent = document.getElementById('last')
+      if (lastComponent) {
+        const lastComponentRect = lastComponent.getBoundingClientRect()
+        const canvasRect = canvasContainer.getBoundingClientRect()
+        const dragY = e.clientY
+        if (dragY > lastComponentRect.bottom && dragY < canvasRect.bottom) {
+          setEndTmpComponent()
+        }
+      }
+    }
+  }
+
   return (
     <div
       id="canvasContainer"
       className={styles.canvas}
       onDragOver={e => e.preventDefault()}
+      onDragEnter={e => handleEnterContainer(e)}
       onDrop={e => handleDrop(e)}
       onDragLeave={e => handleLeave(e)}
     >
-      {componentList.map(componentInfo => {
+      {componentList.map((componentInfo, index) => {
         const { fe_id } = componentInfo
         return (
           <div key={fe_id}>
@@ -147,7 +179,8 @@ const EditCanvas: FC<PropsType> = function ({ loading }: PropsType) {
               </div>
             )}
             <div
-              className={`${styles['canvas-wrapper']} ${selectedId === fe_id ? styles.selected : ''}`}
+              id={index + 1 === componentList.length ? 'last' : undefined}
+              className={`${styles['canvas-wrapper']} ${selectedId === fe_id ? styles.selected : ''} ${fe_id}`}
               onClick={e => handleClick(e, fe_id)}
               onDragEnter={e => handleDragEnter(e, fe_id)}
               onDragOver={e => e.preventDefault()}
